6e111b412ea21084b8ef5c29e27cd5edd20584f4
Ted John <ted@brambles.org>
2017-12-04 21:56:06 +0000

    Spike 1: Add RCT1 toilets JSON

--- src/openrct2/object/Object.h
+++ src/openrct2/object/Object.h
@@ -20,6 +20,7 @@
 
 #ifdef __cplusplus
 
+#include "../core/Json.hpp"
 #include "ImageTable.h"
 #include "StringTable.h"
 
@@ -175,6 +176,7 @@ public:
     const rct_object_entry *    GetObjectEntry() const { return &_objectEntry; }
     virtual void *              GetLegacyData() abstract;
 
+    virtual void ReadJson(IReadObjectContext * context, const json_t * root) { }
     virtual void ReadLegacy(IReadObjectContext * context, IStream * stream) abstract;
     virtual void Load() abstract;
     virtual void Unload() abstract;
